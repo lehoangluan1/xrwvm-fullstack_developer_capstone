f111df7f36e34b64bcecb0cc01482f69601fedeb
Your Name <you@example.com>
2026-04-07 09:45:28 -0400

    Update About page and add about route

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -7,8 +7,7 @@ Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    1. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
@@ -23,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="Home.html")),
+    path('about/', TemplateView.as_view(template_name="About.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
